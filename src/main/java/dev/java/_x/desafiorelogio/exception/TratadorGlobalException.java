1f6eb818f912863f6740ec123d1de4ebc92d51ca
guiantebii <guiantebi2000@gmail.com>
2026-05-05 16:56:43 -0300

    feat: adiciona tratamento global de erros

---
+++ src/main/java/dev/java/_x/desafiorelogio/exception/TratadorGlobalException.java
@@ -0,0 +1,37 @@
+package dev.java._x.desafiorelogio.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+import java.util.List;
+
+@RestControllerAdvice
+public class TratadorGlobalException {
+    @ExceptionHandler(NaoEncontradoException.class)
+    public ResponseEntity<ErroApi> tratarNaoEncontrado(NaoEncontradoException ex, HttpServletRequest req){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErroApi(
+                Instant.now(),404,"Não econtrado", ex.getMessage(),req.getRequestURI(), List.of()
+        ));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErroApi> tratarRequisicaoInvalida(IllegalArgumentException ex, HttpServletRequest req){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErroApi(
+                Instant.now(),400,"Requisicao invalida", ex.getMessage(),req.getRequestURI(), List.of()
+        ));
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErroApi> tratarValidacao(MethodArgumentNotValidException ex, HttpServletRequest req){
+        List<ErroApi.ErroCampo> campos = ex.getBindingResult().getFieldErrors().stream().map(
+                fe -> new ErroApi.ErroCampo(fe.getField(),ex.getMessage()))
+                .toList();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErroApi(
+                Instant.now(),400,"Requisição invalida","Erro de validacao",req.getRequestURI(),campos
+        ));
+    }
+}
